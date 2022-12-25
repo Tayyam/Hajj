ff061fa91e34f99f87c2f4e3bbe0a1407631b4ef
Tayyam <115746736+Tayyam@users.noreply.github.com>
2022-12-26 01:43:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ var countDownDate = new Date("Jun 27, 2023 00:00:00").getTime();
 var x = setInterval(function () {
   // Update the count down every 1 second
 
-  // Get today's date and time
-  var now = new Date().getTime();
+  // create Date object for current location
+  var date = new Date();
+
+  // convert to milliseconds, add local time zone offset and get UTC time in milliseconds
+  var utcTime = date.getTime() + date.getTimezoneOffset() * 60000;
+
+  // KSA GMT +3:00
+  var timeOffset = 3;
+  // create new Date object for a different timezone using supplied its GMT offset.
+  var ksaTime = utcTime + 3600000 * timeOffset;
 
   // Find the distance between now and the count down date
-  var distanceDays = countDownDate - now;
+  var distanceDays = countDownDate - ksaTime;
 
   // Time calculations for days, hours, minutes and seconds
   // days
